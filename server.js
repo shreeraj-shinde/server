d1a4513a7fb5e36899dbd6aeee8930f9665843a0
shreeraj-shinde <shreerajshinde86@gmail.com>
2022-08-22 00:32:51 +0530

    todo server

--- server.js
+++ server.js
@@ -2,10 +2,36 @@ const http = require("http");
 
 const port = 8081;
 
+const todoList = ["Complete Node byte", "Play Cricket"];
+
 http
   .createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(`<h1>Hello </h1>`);
+    const { method, url } = request;
+
+    if (url === "/todo") {
+      if (method === "GET") {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(todoList.toString());
+      } else if (method === "POST") {
+        let body = "";
+        request
+          .on("error", (err) => {
+            console.error(err);
+          })
+          .on("data", (chunk) => {
+            body = body + chunk;
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+            console.log(body);
+          });
+      } else {
+        response.writeHead(404);
+      }
+    } else {
+      response.writeHead(404);
+    }
+
     response.end();
   })
 
